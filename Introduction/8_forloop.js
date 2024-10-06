f7cd34a0cb74abd3202fadfe14afbb687b82d506
Yash <yashuan0202@gmail.com>
2024-10-07 00:49:36 +0530

    Update 8_forloop.js

--- Introduction/8_forloop.js
+++ Introduction/8_forloop.js
@@ -8,3 +8,10 @@ for(var i = 10; i>0; i--) {
     console.log(i);
 }
 console.log('Happy New Year');
+
+//----------------------------------------
+
+var cubes = 'ABCDEFG';
+for (var i = 0; i < cubes.length; i++) {
+    console.log(cubes[i])
+}
