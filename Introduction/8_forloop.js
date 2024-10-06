f2d0fd3902448043118b6a9f4731eae863a984b5
Yash <yashuan0202@gmail.com>
2024-10-07 00:09:54 +0530

    Add files via upload

---
+++ Introduction/8_forloop.js
@@ -0,0 +1,10 @@
+for(var i = 1; i<=10; i++) {
+    console.log(i,"Go");
+}
+
+//--------------------------------------
+
+for(var i = 10; i>0; i--) {
+    console.log(i);
+}
+console.log('Happy New Year');
