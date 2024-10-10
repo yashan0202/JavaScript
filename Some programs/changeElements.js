e1e53d125c3e73fa06d08a281274da1dd2406a03
Yash <yashuan0202@gmail.com>
2024-10-10 20:34:33 +0530

    Add files via upload

---
+++ Some programs/changeElements.js
@@ -0,0 +1,13 @@
+var h1 = document.createElement('h1')
+h1.innerText = "Type into the input to make this text change"
+
+var input = document.createElement('input')
+input.setAttribute('type', 'text')
+
+document.body.innerText = '';
+document.body.appendChild(h1);
+document.body.appendChild(input);
+
+input.addEventListener('change', function() {
+    h1.innerText = input.value
+})
